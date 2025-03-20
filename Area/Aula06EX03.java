dcf9a4009f8f037f58b4b72932329ed2bcd5be9d
Guilherme-Antoniassi <145165615+Guilherme-Antoniassi@users.noreply.github.com>
2025-03-20 15:23:06 -0300

    Add files via upload

---
+++ Area/Aula06EX03.java
@@ -0,0 +1,18 @@
+
+package aula06ex03;
+
+/**
+ *
+ * @author unifgaraujo
+ */
+public class Aula06EX03 {
+
+    public static void main(String[] args) {
+      
+        Area area=new Area();
+        area.imprimirArea(8);
+        area.imprimirArea(4, 7);
+        
+    }
+    
+}
