48a59aeb6656efb374ac6bbbd8fa380ce5edb452
Guilherme-Antoniassi <145165615+Guilherme-Antoniassi@users.noreply.github.com>
2025-04-15 18:46:49 -0300

    Add files via upload

---
+++ JAVA/Aula11ex01/aula11ex01/Aula11ex01.java
@@ -0,0 +1,20 @@
+
+package aula11ex01;
+
+import View.Loggin_Frame;
+
+/**
+ *
+ * @author unifgaraujo
+ */
+public class Aula11ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Loggin_Frame fr=new  Loggin_Frame();
+       fr.setVisible(true);
+    }
+    
+}
